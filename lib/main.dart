d30998b7c7a25ccfcc4be11a7f89609985df5a11
khady <khadimbacke734@gmail.com>
2025-12-10 02:08:57 +0000

    ajout du chaps de sis et du boutton CTA

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'theme/app_theme.dart';
+import 'widgets/cta_section.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,10 +15,12 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   ThemeMode _mode = ThemeMode.system;
+  var conteur = 0;
 
   // Fonction appelée lorsqu'on change le toggle
   void changeTheme(bool isDark) {
     setState(() {
+      //on execute les themes predefinie sur app_theme.dart dependant de la valeur de isDark
       _mode = isDark ? ThemeMode.dark : ThemeMode.light;
     });
   }
@@ -47,12 +50,14 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   bool isDark = false;
+  final TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("COFFE SHOP"),
+
         ////mon toggle
         actions: [
           Icon(
@@ -60,22 +65,59 @@ class _MyHomePageState extends State<MyHomePage> {
             isDark ? Icons.dark_mode : Icons.light_mode,
           ),
           Switch(
+            ///le theme est capte au niveau de isDark
             value: isDark,
+            //on click la valueur change
             onChanged: (value) {
               setState(() {
+                //isDark recoit la nouvelle valeur:soit true,soit false
                 isDark = value;
               });
-
               widget.onThemeChanged(value);
             },
           ),
         ],
+        ////bouton recherche
       ),
 
       body: Center(
-        child: Text(
-          "Theme changer ",
-          style: Theme.of(context).textTheme.headlineMedium,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              "Theme changer ",
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 50),
+            Container(
+              width: 300.0,
+              margin: EdgeInsets.only(left: 600.0),
+              child: TextField(
+                //:pour preparer la saisie
+                keyboardType: TextInputType.text,
+                // obscureText: true,: pour cacher instatenement le texte saisieRR
+                //pour limiter
+                maxLength: 20,
+                controller: _controller, // Associe le controller à ce TextField
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 8.0,
+                    horizontal: 10.0,
+                  ),
+                  //labelText: 'Entrez votre texte',
+                  hintText: 'Rechercher un café...',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ), // Coins arrondis
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 280),
+            CtaButton(),
+          ],
         ),
       ),
     );
---
+++ lib/widgets/cta_section.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CtaButton extends StatefulWidget {
+  const CtaButton({super.key});
+
+  @override
+  State<CtaButton> createState() => _CtaButtonState();
+}
+
+class _CtaButtonState extends State<CtaButton> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        setState(() {
+          counter++;
+        });
+      },
+      child: Text("Ajoutee: $counter"),
+    );
+  }
+}
